9adc3f35661102cda01c2cab61c08f4d85b7445f
Alexander Ho <46589739+alx00ho@users.noreply.github.com>
2023-07-18 23:07:34 -0400

    Functional Frontend
    Introduced functionality for adding, removing, and getting goals to add to the state

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -40,16 +40,14 @@ const updateGoal = asyncHandler(async (req, res) => {
     throw new Error("Goal not found");
   }
 
-  const User = await User.findById(req.user.id);
-
   // Check for user
-  if (!user) {
+  if (!req.user) {
     res.status(401);
     throw new Error("User Not Found");
   }
 
   // Make sure the logged in user matches the goal user
-  if (goal.user.toString() !== user.id) {
+  if (goal.user.toString() !== req.user.id) {
     res.status(401);
     throw new Error("User Not Authorized");
   }
@@ -72,16 +70,14 @@ const deleteGoal = asyncHandler(async (req, res) => {
     throw new Error("Goal not found");
   }
 
-  const User = await User.findById(req.user.id);
-
   // Check for user
-  if (!user) {
+  if (!req.user) {
     res.status(401);
     throw new Error("User Not Found");
   }
 
   // Make sure the logged in user matches the goal user
-  if (goal.user.toString() !== user.id) {
+  if (goal.user.toString() !== req.user.id) {
     res.status(401);
     throw new Error("User Not Authorized");
   }
